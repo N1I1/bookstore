364dd379f9d588f70ff871d98cb5643f51340cfb
N1I1 <156334744+N1I1@users.noreply.github.com>
2025-06-18 21:59:11 +0800

    fix: fix for some bugs (#87)

--- backend/app/routes/auth/admin_login.py
+++ backend/app/routes/auth/admin_login.py
@@ -16,11 +16,11 @@ class AdminLoginView(MethodView):
     def post(self):
         # 获取表单数据
         data = request.json
-        username = data.get('adminname', None)
+        username = data.get('username', None)
         password = data.get('password', None)
 
         if not username or not password:
-            return jsonify({"error": "Missing adminname or password"}), 400
+            return jsonify({"error": "Missing username or password"}), 400
 
         try:
             admin_user = Admin.query.filter_by(username=username).first()
@@ -30,7 +30,7 @@ class AdminLoginView(MethodView):
                 set_user_session(admin_user)
                 return jsonify({"message": "Login successful"}), 200
             else:
-                return jsonify({"error": "Invalid adminname or password"}), 401
+                return jsonify({"error": "Invalid username or password"}), 401
         except Exception as e:
             db.session.rollback()
             ################
