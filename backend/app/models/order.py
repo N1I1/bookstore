09c757467e335d3a8e902d3c7e13cef734c5004b
N1I1 <he231920@gmail.com>
2025-06-18 14:33:04 +0800

    fix: add is_deleted

--- backend/app/models/order.py
+++ backend/app/models/order.py
@@ -5,7 +5,8 @@ from datetime import datetime
 class Order(db.Model):
     __tablename__ = 'order'
     order_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    # 管理员被删除时，设置为NULL
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete='SET NULL'), nullable=True)
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.admin_id'), nullable=True)
     order_status = db.Column(db.Enum('未支付', '已支付', '已发货', '已完成', '订单取消', name='order_status'), nullable=False, default='未支付')
     order_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
@@ -19,6 +20,8 @@ class Order(db.Model):
     biller_phone = db.Column(db.String(20), nullable=False)
     remark = db.Column(db.Text, default=None)
     total_amount = db.Column(db.Numeric(10, 2), nullable=False, default=0.00) # Total amount of the order
+    is_deleted = db.Column(db.Boolean, default=False)  # 软删除
 
+    # 删除订单时，级联删除订单详情
     order_details = db.relationship('OrderDetail', backref='order', lazy='dynamic', cascade='all, delete-orphan')
     user = db.relationship('User', backref='orders', lazy='joined')
