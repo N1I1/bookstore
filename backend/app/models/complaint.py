95a9547f00438532669dfe2720d68760f1b84385
N1I1 <he231920@gmail.com>
2025-06-19 10:48:42 +0800

    fix: fix bug in complaint

--- backend/app/models/complaint.py
+++ backend/app/models/complaint.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class Complaint(db.Model):
     __tablename__ = 'complaint'
     complaint_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete="SET NULL"), nullable=True)
     content = db.Column(db.Text, nullable=False)
     complaint_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    status = db.Column(db.Enum('待处理', '已受理' '已解决', name='complaint_status'), nullable=False, default='待处理')
+    status = db.Column(db.Enum('待处理', '已受理', '已解决', name='complaint_status'), nullable=False, default='待处理')
     result = db.Column(db.Text, default=None)
